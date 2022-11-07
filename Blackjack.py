226e33106113a0dca55290da8f97b2a49dc2a246
nazarkirilloff <117318398+nazarkirilloff@users.noreply.github.com>
2022-11-07 18:47:40 +0100

    Simplified code for deck, added Player draw, show hand, and count points methods

--- Blackjack.py
+++ Blackjack.py
@@ -1,20 +1,20 @@
-class Card:
-    def __init__(self, suit, value):
-        self.suit = suit
-        self.value = value
-
-    def show(self):
-        print ("{} of {}".format(self.value, self.suit))
+from random import shuffle
 
 class Deck:
+
     def __init__(self):
-        self.cards = []
+        self.deck_to_play = []
         self.build_deck()
 
     def build_deck(self):
-        for suit in ['Spades', 'Clubs', 'Diamonds', 'Hearts']:
-            for value in [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']:
-                self.cards.append(Card(suit, value))
+        self.deck_to_play = [f'{value} of {suit}' for suit in ['Spades', 'Clubs', 'Diamonds', 'Hearts'] for value in [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']]
+        shuffle(self.deck_to_play)
+
+    def show(self):
+        print(self.deck_to_play)
+
+    def draw(self):
+        return self.deck_to_play.pop(0)
 
 class Player:
     pass
@@ -22,5 +22,7 @@ class Player:
 class Game:
     pass
 
-card1 = Card('Spades', 'King')
-card1.show()
+deck = Deck()
+deck.show()
+
+dealer = Player()

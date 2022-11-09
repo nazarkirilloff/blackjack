82a81bb64940c08f1012914494408e100baa85e8
nazarkirilloff <117318398+nazarkirilloff@users.noreply.github.com>
2022-11-09 15:22:29 +0100

    Changed the else condition in the Game insurance method to the right one

--- Blackjack.py
+++ Blackjack.py
@@ -125,8 +125,7 @@ class Game:
                     '>>>>> ')
             player1.bet(insurance_bet)
         else:
-            print('Unfortunately you do not have any sufficient funds to make an insurance bet.\n'
-                  'You will have to play with your current bet.\n'
+            print('You will play with your current bet.\n'
                   '/////////')
 
     def dealer_play(self):
